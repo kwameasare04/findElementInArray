bef064870e5afb6ab923b048493a3e485f2c9ae5
kwameasare04 <73545744+kwameasare04@users.noreply.github.com>
2021-04-06 15:31:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const transport = ["Bus", "Car", "Bicycle", "Airplane"];
+
+/* Create a function "elementFound" with two parameters "inputArray" and "searchElement".
+If "searchElement" is found in the "inputArray" - return "true".
+Otherwise return "false"
+
+Hint: Use "some" helper method to solve this
+*/
+
+const elementFound = (inputArray, searchElement) => {
+    return inputArray.some(element => element === searchElement);
+};
+
+
+console.log(elementFound(transport, "Bus")); // true
+console.log(elementFound(transport, "Phone")); // false
+console.log(elementFound(transport, "Airplane")); // true
